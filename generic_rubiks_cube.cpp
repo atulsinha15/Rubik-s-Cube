b648792f278cf31a25aafbb581f3a0de440c896b
atulsinha15 <atulsinha803115@gmail.com>
2024-06-21 20:47:36 +0530

    add to generic model

---
+++ generic_rubiks_cube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 21-06-2024.
+//
+
+#include "generic_rubiks_cube.h"
---
+++ generic_rubiks_cube.h
@@ -0,0 +1,16 @@
+//
+// Created by HP on 21-06-2024.
+//
+
+#ifndef GENERIC_RUBIKS_CUBE_H
+#define GENERIC_RUBIKS_CUBE_H
+
+
+
+class generic_rubiks_cube {
+
+};
+
+
+
+#endif //GENERIC_RUBIKS_CUBE_H
